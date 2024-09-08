a0cdb0e810d12f52c4396d20fa7c0026bc851a72
iayr1 <mayurchaudhari1675@gmail.com>
2024-09-08 18:39:47 +0530

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ void main() async {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (context) => AuthProvider()),
-        // Add other providers as needed
       ],
       child: MyApp(),
     ),
